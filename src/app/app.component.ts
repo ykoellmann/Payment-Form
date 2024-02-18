034e4c7f04d785e3530a8aa74f36aaf5fd0624ee
yankoePC <yankoe@outlook.de>
2024-02-18 20:22:02 +0100

    feat: create form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CurrencyPipe, DatePipe, TitleCasePipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TitleCasePipe, DatePipe, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+  date: string = '';
+  amount: string = '';
+
+  onNameChange(input: string): void {
+    this.name = input;
+  }
+
+  onDateChange(input: string): void {
+    this.date = input;
+  }
+
+  onAmountChange(input: string): void {
+    this.amount = input;
+  }
 }
